5d7671c338ea1bad3bf5a81628a5f20212fb900a
Hari Kammana <javahome.git@gmail.com>
2019-06-06 10:48:09 +0530

    session-3

--- vpc.tf
+++ vpc.tf
@@ -19,3 +19,36 @@ resource "aws_subnet" "main" {
   availability_zone = "${element(local.azs, count.index)}"
   tags              = "${var.public_sub_tags}"
 }
+
+# Create Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.myapp_vpc.id}"
+
+  tags = {
+    Name = "javhome-igw"
+  }
+}
+
+# Create custom route table for public subent
+
+resource "aws_route_table" "r" {
+  vpc_id = "${aws_vpc.myapp_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name = "public-routetable"
+  }
+}
+
+# associate public subnets with public route table
+
+resource "aws_route_table_association" "a" {
+  count          = "${length(local.azs)}"
+  subnet_id      = "${element(aws_subnet.main.*.id, count.index)}"
+  route_table_id = "${aws_route_table.r.id}"
+}
